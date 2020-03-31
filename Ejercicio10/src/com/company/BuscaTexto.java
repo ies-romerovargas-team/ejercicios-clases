31974caf61667375795b16ba2e9b4896def8a7db
davidbermudez <davidbermudez@jerez.es>
2020-03-31 16:16:41 +0200

    Finalizado ejercicio 10

--- Ejercicio10/src/com/company/BuscaTexto.java
+++ Ejercicio10/src/com/company/BuscaTexto.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class BuscaTexto {
+
     private String texto;
     private int puntero;
 
@@ -19,7 +20,7 @@ public class BuscaTexto {
     }
 
     // Métodos
-    private void cargaFicheros(String fichero)
+    public void cargaFicheros(String fichero)
     {
         // lee el fichero de texto que le pasamos y guarda su contenido en el atributo texto
         String textFile = "";
@@ -46,22 +47,84 @@ public class BuscaTexto {
         this.texto = textFile;
     }
 
-    private void reinicia()
+    public void reinicia()
     {
         // pone el puntero a 0
         this.puntero = 0;
     }
 
-    private boolean busca(String cadena)
+    public boolean busca(String cadena)
     {
         // busca la cadena dentro del texto y guarda esa posición en el puntero. Si la cadena no se encuentra, no
         // tocaremos el puntero y devolveremos false. Siempre buscará desde el principio.
         String texto = this.texto;
         int encuentra = texto.indexOf(cadena);
-        if (encuentra==0) return false;
+        if (encuentra == 0) {
+            return false;
+        }
         this.puntero = encuentra;
         return true;
     }
 
+    public boolean buscaSiguiente(String cadena)
+    {
+        // busca la cadena dentro del texto a partir de la posición actual del puntero
+        // y guarda el resultado en el puntero. Si no se encuentra, no tocamos el puntero y devolvemos false.
+        String texto = this.texto;
+        int encuentra = texto.indexOf(cadena, this.puntero);
+        if (encuentra == 0) {
+            return false;
+        }
+        this.puntero = encuentra;
+        return true;
+    }
+
+    public String extraeCadena(String delimitador1, String delimitador2)
+    {
+        // A partir de la posición  actual  del  puntero,  busca  el delimitador1.
+        // A  partir  de  ahí  irá guardando todos los caracteres que encuentre en una cadena hasta llegar al delimitador2
+        // Devolverá la cadena obtenida. Si no se encuentra alguno de los dos delimitadores, nos devuelve null
+        String devuelve = null;
+        if (buscaSiguiente(delimitador1))
+        {
+            int inicio = this.puntero + delimitador1.length();
+            if(buscaSiguiente(delimitador2))
+            {
+                int fin = this.puntero;
+                devuelve = this.texto.substring(inicio, fin);
+            }
+        }
+        return devuelve;
+    }
+
+    public String extraeCadenaConDelim(String delimitador1, String delimitador2)
+    {
+       // igual  que la  anterior,  pero en la cadena que nos devuelve incluye los delimitadores
+        String devuelve = null;
+        if (buscaSiguiente(delimitador1))
+        {
+            int inicio = this.puntero;
+            if(buscaSiguiente(delimitador2))
+            {
+                int fin = this.puntero + delimitador2.length();
+                devuelve = this.texto.substring(inicio, fin);
+            }
+        }
+        return devuelve;
+    }
+
+    public int getPuntero() {
+        return puntero;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setPuntero(int puntero) {
+        if (puntero < 0) puntero = 0;
+        if (puntero > this.texto.length()) puntero = this.texto.length();
+        this.puntero = puntero;
+    }
 
 }
--- Ejercicio10/src/com/company/Main.java
+++ Ejercicio10/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        BuscaTexto text = new BuscaTexto();
+        text.cargaFicheros("cambio.html");
+        String result = text.extraeCadena("<div id=\"cc-ratebox\" name=\"cc-ratebox\" aria-labelledby=\"elb\" tabindex=\"4\">", "</div>");
+        System.out.println(result);
+        text.reinicia();
+        result = text.extraeCadenaConDelim("<div id=\"cc-ratebox\" name=\"cc-ratebox\" aria-labelledby=\"elb\" tabindex=\"4\">", "</div>");
+        System.out.println(result);
     }
 }
